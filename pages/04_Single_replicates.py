be47fe609ffc70493b552cf23121eb2c638286da
ASintsova <ansintsova@biol.ethz.ch>
2025-01-16 16:44:02 +0100

    Fixing bugs in upset plots, adding option to set category order

--- pages/04_Single_replicates.py
+++ pages/04_Single_replicates.py
@@ -2,15 +2,19 @@ import streamlit as st
 import pandas as pd
 from upsetplot import plot
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def generate_upset_plot(df):
+def generate_upset_plot(df, cat_order):
     unique_values = pd.unique(df.values.ravel())
+    unique_values = [v for v in unique_values if v is not np.nan]
     result = pd.DataFrame(
         {val: df.apply(lambda col: col == val).any(axis=0) for val in unique_values},
         index=df.columns
     ).T
-    plot(result.value_counts())
+    result = result[cat_order]
+    result_vc = result.value_counts()
+    plot(result_vc, sort_categories_by='-input')
     fig = plt.gcf()  # Get the current figure
     fig.set_dpi(300)
     fig.set_size_inches(6, 4) 
@@ -25,19 +29,23 @@ def app():
                                "a,b,b,d".split(","), "b,a,c,e".split(","), "f,g,f,h".split(","),
                                "i,i,i,i".split(","), "g,k,,l".split(","), "m,n,,".split(",")], 
                                columns=['Rep1', 'Rep2', 'Rep3', 'Pooled'])
-    st.caption("The file can have any number of columns, and column labels do NOT need to match the example")
+    st.markdown("The file can have any number of columns, and column labels do NOT need to match the example")
+    st.markdown("Here is an example dataframe:")
     st.dataframe(upset_df)
     hits_file = st.file_uploader('File upload')
-
     if not hits_file:
         st.warning("No file uploaded. Showing an example UpSet plot.")
     else:
         upset_df = pd.read_csv(hits_file)
-    fig, result = generate_upset_plot(upset_df)
+    selection = st.pills("Select category order:", upset_df.columns, selection_mode="multi", default=upset_df.columns.sort_values())
+    st.markdown(f"Your selected category orders: {selection}.")
+    if len(selection) < 2:
+        st.warning("Cannot draw upset plot for 1 category, select more categories")
+        st.stop()
+    fig, result = generate_upset_plot(upset_df, selection)
     st.pyplot(fig)
-
     st.markdown("#### Show specific intersections")
-    reps = st.multiselect(label='Choose intersection to show', options=['All'] + list(upset_df.columns), default='All')
+    reps = st.multiselect(label='Choose intersection to show', options=['All'] + list(result.columns), default='All')
     if 'All' in reps:
         st.write(result[result.index != ''])
     else:
